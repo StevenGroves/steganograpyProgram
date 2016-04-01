9b51799326a4794224cce9e6821ac70b9649ecbb
Raquel_Figueroa <raquelifigueroa@student.hartnell.edu>
2016-03-31 22:52:51 -0700

    Combined functions to encrypt and decrypt text and photos.

--- project2.py
+++ project2.py
@@ -1,7 +1,102 @@
+'''Team #44, Steganogasaurus
+Members: Giselle Beltran
+		 Raquel Figueroa 
+		 Steven Groves
+		 Brian Little
+'''
+
 from Tkinter import *
+import tkFileDialog
+from PIL import Image, ImageOps
+import tkMessageBox
 
 def nextFrame(frame):
 	frame.tkraise()
+
+def coverimage():
+	filename = tkFileDialog.askopenfilename()
+	return filename
+
+def secretimage():
+	filename = tkFileDialog.askopenfilename()
+	return filename
+
+def hideImage(s = 4):
+	data = Image.open(coverimage())
+	key = ImageOps.autocontrast(Image.open(secretimage()).resize(data.size))
+	for x in range(data.size[0]):
+		for y in range(data.size[1]):
+			p = data.getpixel((x,y))
+			q = key.getpixel((x,y))
+			red = p[0] - (p[0] % s) + (s * q[0] / 255)
+			green = p[1] - (p[1] % s) + (s * q[1] / 255)
+			blue = p[2] - (p[2] % s) + (s * q[2] / 255)
+			data.putpixel((x,y), (red, green, blue))
+	data.save("hidden.png")
+	data.show()
+	nextFrame(f7)
+
+def extract_image(from_image, s=4):
+	hiddenImg = Image.open(from_image)
+	for x in range(hiddenImg.size[0]):
+		for y in range(hiddenImg.size[1]):
+			p = hiddenImg.getpixel((x,y))
+			red = (p[0] % s) * 255 / s
+			green = (p[1] % s) * 255 / s
+			blue = (p[2] % s) * 255 / s
+			hiddenImg.putpixel((x,y), (red, green, blue))
+	#return data
+	hiddenImg.save("secretImg.png")
+	hiddenImg.show()
+	nextFrame(f11)
+
+
+#Used this website for reference on encoding strings into images: 
+#https://www.daniweb.com/programming/software-development/code/485063/hide-private-message-in-an-image-python
+	
+def hideString():
+	data = e.get()
+	datalength = len(data)
+	image = Image.open(coverimage())
+	height, width = image.size
+	newimg = image.copy()
+	
+	count = 0
+	for x in range(0, height):
+		for y in range(0, width):
+			red, green, blue = newimg.getpixel((x, y))
+			if x ==0 and y == 0:
+				new_val = datalength #Very first red pixel will hold the value of the length of the string
+			elif count <= datalength:
+				temp = data[count - 1]
+				new_val = ord(temp) + 3 #the +3 is just to make it less obvious to decrypt
+			else:
+				new_val = red
+			newimg.putpixel((x, y), (new_val, green, blue))
+			count += 1
+	newimg.save("hiddentext.png")
+	newimg.show()
+	nextFrame(f6)
+			
+def decodeString():
+	image = Image.open(coverimage())
+	height, width = image.size
+
+	msg = ""
+	count = 0
+	for x in range(0, height):
+		for y in range(0, width):
+			red, green, blue = image.getpixel((x, y))
+			if x == 0 and y == 0: #We assume the first red value holds the length of the string we're decoding
+				length = red
+			elif count <= length:
+				msg += unichr(red - 3) #-3 to put the value back to original state
+			if count > length:
+				print "Secret Message: " + msg
+				nextFrame(f9)
+				return msg
+			count += 1
+			
 
 root = Tk()
 
@@ -9,12 +104,20 @@ root.configure(bg = 'DodgerBlue4')
 root.title("Encrypt or Decrypt")
 root.geometry('{}x{}'.format(425, 300))
 
-f1 = Frame(root)
-f2 = Frame(root)
-f3 = Frame(root)
-#f4 = Frame(root)
+f1 = Frame(root) # Main menu
+f2 = Frame(root) # Encrypt menu
+f3 = Frame(root) # Decrypt menu
+f4 = Frame(root) # Encrypt photo
+f5 = Frame(root) # Encrypt text
+f6 = Frame(root) # successful text encrypted
+f7 = Frame(root) # successful photo encrypted
+f8 = Frame(root) # Decrypt text from photo
+f9 = Frame(root) # Successful text decryption
+f10 = Frame(root) # Decrypt photo from photo
+f11 = Frame(root) # Successful photo decryption
 
-for frame in (f1, f2, f3):
+
+for frame in (f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11):
 	frame.grid(row = 0, column = 0, sticky = 'news')
 
 ##############################################################
@@ -25,7 +128,7 @@ Label(f1,
 		bg = 'DodgerBlue4',
 		font = 'times 18 bold',
 		pady = 60,
-		padx = 80).pack()
+		padx = 70).pack()
 #encode button
 encodebutton = Button(f1,
 						text = 'Encrypt',
@@ -58,7 +161,7 @@ encryptTextButton = Button(f2,
 						bg = 'azure',
 						fg = 'turquoise4',
 						width = 12,
-						command = lambda:nextFrame(f1))
+						command = lambda:nextFrame(f5))
 encryptTextButton.pack(padx = 10, side = LEFT)
 
 #decode button
@@ -67,7 +170,7 @@ encryptPhotoButton = Button(f2,
 						bg = 'turquoise4',
 						fg = 'azure',
 						width = 12,
-						command = lambda:nextFrame(f1))
+						command = lambda:nextFrame(f4))
 encryptPhotoButton.pack(padx = 10, side = LEFT)
 
 #return to main menu button
@@ -93,8 +196,8 @@ decryptTextButton = Button(f3,
 						bg = 'azure',
 						fg = 'turquoise4',
 						width = 7,
-						command = lambda:nextFrame(f1))
-decryptTextButton.pack(padx = 9, side = LEFT)
+						command = lambda:nextFrame(f8))
+decryptTextButton.pack(padx = 30, side = LEFT)
 
 #decrypt photo button
 decryptPhotoButton = Button(f3,
@@ -102,17 +205,8 @@ decryptPhotoButton = Button(f3,
 						bg = 'turquoise4',
 						fg = 'azure',
 						width = 7,
-						command = lambda:nextFrame(f1))
-decryptPhotoButton.pack(padx = 9, side = LEFT)
-
-#decrypt unknown button
-decryptUnknownButton = Button(f3,
-						text = 'Unknown',
-						bg = 'slateblue4',
-						fg = 'antiquewhite1',
-						width = 7,
-						command = lambda:nextFrame(f1))
-decryptUnknownButton.pack(padx = 9, side = LEFT)
+						command = lambda:nextFrame(f10))
+decryptPhotoButton.pack(padx = 30, side = LEFT)
 
 #return to main menu button
 mainMenuButton = Button(f3,
@@ -121,7 +215,214 @@ mainMenuButton = Button(f3,
 						fg = 'red3',
 						width = 8,
 						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 30, side = LEFT)
+#####################################################################
+#f4 - Encrypt picture into picture
+Label(f4,
+		text = "Hide a Photo in a Photo\nFirst, pick the cover image\nThen, pick the secret image",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 75,
+		padx = 81).pack()
+#encrypt button
+encryptButton = Button(f4,
+						text = 'Encrypt',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 7,
+						command = lambda:hideImage())
+encryptButton.pack(padx = 70, side = LEFT)
+#back button
+backButton = Button(f4,
+						text = 'Back',
+						bg = 'azure',
+						fg = 'turquoise4',
+						width = 7,
+						command = lambda:nextFrame(f2))
+backButton.pack(padx = 70, side = RIGHT)
+
+#######################################################################
+#Frame 5 - Hide Text in a Photo
+Label(f5,
+		text = 'Enter the text you\n would like to encode:',
+		font = 'times 20 bold',
+		bg = 'DodgerBlue4',
+		fg = 'white',
+		pady = 70,
+		padx = 98).pack()
+
+#Text box		
+e = Entry(f5, bd = 5)
+e.pack()
+	
+encryptStringButton = Button(f5,
+						text ='Submit\nText',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 8,
+						command = lambda:hideString())
+encryptStringButton.pack(padx = 10, side = LEFT)
+		
+mainMenuButton = Button(f5,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 12,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 10, side = RIGHT)
+#######################################################################
+#f6 - Show successful text encrypting
+Label(f6, 
+		text = "Text Successfully\nEncrypted in Photo.\nFile in Directory.",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 60,
+		padx = 95).pack()
+
+mainMenuButton = Button(f6,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 12,
+						command = lambda:nextFrame(f1))
 mainMenuButton.pack(padx = 10, side = LEFT)
+
+quitbutton = Button(f6,
+						text = 'Quit',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 13,
+						command = lambda:root.quit())
+quitbutton.pack(padx = 50, side = RIGHT)
+########################################################################
+#f7 - Show successful photo encrypting
+Label(f7, 
+		text = "Photo successfully\nencrypted in photo.\nFile in directory.",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 80,
+		padx = 98).pack()
+
+mainMenuButton = Button(f7,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 12,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 50, side = LEFT)
+
+quitbutton = Button(f7,
+						text = 'Quit',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 13,
+						command = lambda:root.quit())
+quitbutton.pack(padx = 50, side = RIGHT)
+########################################################################
+#f8 - Decrypt text from a picture
+Label(f8,
+		text = "Decrypt Text From a Photo.\n Choose a File:",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 95,
+		padx = 79).pack()
+#Decrypt text from photo button
+decryptTextImage = Button(f8,
+						text = 'Choose Photo\nFrom a File',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 12,
+						command = lambda:decodeString())
+decryptTextImage.pack(padx = 50, side = LEFT)
+#return to main menu button
+mainMenuButton = Button(f8,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 10,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 50, side = LEFT)
+#################################################################
+#f9 - Show successful text decrypting
+Label(f9, 
+		text = "Text successfully\ndecrypted from photo.",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 90,
+		padx = 100).pack()
+
+mainMenuButton = Button(f9,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 12,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 10, side = LEFT)
+
+quitbutton = Button(f9,
+						text = 'Quit',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 13,
+						command = lambda:root.quit())
+quitbutton.pack(padx = 50, side = RIGHT)
+########################################################################
+#f10 - Decrypt photo from a picture
+Label(f10,
+		text = "Decrypt Photo From a Photo.\n Choose a File:",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 95,
+		padx = 79).pack()
+#Decrypt text from photo button
+decryptTextImage = Button(f10,
+						text = 'Choose Photo\nFrom a File',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 12,
+						command = lambda:extract_image(coverimage()))
+decryptTextImage.pack(padx = 50, side = LEFT)
+#return to main menu button
+mainMenuButton = Button(f10,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 10,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 50, side = LEFT)
+#################################################################
+#f11 - Show successful photo decrypting
+Label(f11, 
+		text = "Hidden photo successfully\ndecrypted from photo.",
+		fg = 'white',
+		bg = 'DodgerBlue4',
+		font = 'times 18 bold',
+		pady = 90,
+		padx = 100).pack()
+
+mainMenuButton = Button(f11,
+						text = 'Return to\nMain Menu',
+						bg = 'LightCyan',
+						fg = 'red3',
+						width = 12,
+						command = lambda:nextFrame(f1))
+mainMenuButton.pack(padx = 10, side = LEFT)
+
+quitbutton = Button(f11,
+						text = 'Quit',
+						bg = 'turquoise4',
+						fg = 'azure',
+						width = 13,
+						command = lambda:root.quit())
+quitbutton.pack(padx = 50, side = RIGHT)
+########################################################################
+
 
 nextFrame(f1)
 
